50ba3e771833203a892f65b8486c7c5a2e25eec3
jonas <jonas.masharqa@gmail.com>
2019-12-21 18:27:30 +0100

    Adds routes for Projects, LandingPage and AboutMe components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import LandingPage from './Components/LandingPage'
+import Projects from './Components/Projects'
+import AboutMe from './Components/AboutMe'
 import Sidebar from './Components/Sidebar'
+import { Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
       <Sidebar />
-      <LandingPage />
+      <Route exact path="/" component={LandingPage} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/about" component={AboutMe} />
     </>
   )
 }
