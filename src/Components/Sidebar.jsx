463886cbfed10e882b400c7201bb86dbdcafb673
jonas <jonas.masharqa@gmail.com>
2019-12-21 18:33:46 +0100

    Adds NavLinks in sidebar.

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Menu } from 'semantic-ui-react'
 
 const Sidebar = () => {
   return (
     <>
-      <p>sidebar</p>
+      <Menu text vertical id='sidebar'>
+        <Menu.Item header 
+          className='sidebar-item'
+          id='sidebar-header'
+          name='Jonas Masharqa'
+          as={NavLink}
+          to='/'
+        />
+        <Menu.Item 
+          className='sidebar-item'
+          name='Projects'
+          as={NavLink}
+          to='/projects'
+        />
+        <Menu.Item 
+          className='sidebar-item'
+          name='About me'
+          as={NavLink}
+          to='/about'
+        />
+      </Menu>
     </>
   )
 }
