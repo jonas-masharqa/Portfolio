149805b002a5e7280bb6f67014a2de6a744803bf
jonas <jonas.masharqa@gmail.com>
2020-01-04 19:59:52 +0100

    Changes Projecs component to class
    - Starts projects mapping
    - Creates data json folder for projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Projects = () => {
-  return (
-    <>
-      
-    </>
-  )
+class Projects extends Component {
+  state = {
+    projects: []
+  }
+
+  render() {
+    const projects = this.state.projects
+    let projectsList
+
+    if (projects.length > 0) {
+      projectsList = projects.map(project => {
+        return (
+          <div key={project.id}>
+            <h3 className="ui header">{project.name}</h3>
+          </div>
+        )
+      })
+    }
+
+    return (
+      <>
+        <div className="ui main container">
+          <h1 className="ui header">My Projects</h1>
+          {projectsList}
+        </div>
+      </>
+    )
+  }
 }
 
 export default Projects
