426838c36bf0ae8bb7e0ce5f37fd1737dac2e49d
jonas <jonas.masharqa@gmail.com>
2020-01-05 18:13:53 +0100

    About me
    - Adds picture and text for about me page
    - Adds styling for about me page

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import LandingPage from './Components/LandingPage'
 import Projects from './Components/Projects'
 import AboutMe from './Components/AboutMe'
 import Sidebar from './Components/Sidebar'
+import Contact from './Components/Contact'
 import { Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
       <Sidebar />
-      <Route exact path="/" component={LandingPage} />
-      <Route exact path="/projects" component={Projects} />
-      <Route exact path="/about" component={AboutMe} />
+      <Route exact path="/" component={LandingPage}/>
+      <Route exact path="/projects" component={Projects}/>
+      <Route exact path="/about" component={AboutMe}/>
+      <Route exact path="/contact" component={Contact}/>
     </>
   )
 }
