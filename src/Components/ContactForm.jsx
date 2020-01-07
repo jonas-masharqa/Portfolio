3b1dfe22e61bec0f4771f1d069c81257839c4cf4
jonas <jonas.masharqa@gmail.com>
2020-01-07 16:33:59 +0100

    Adds new HTML and css for contact form.

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,55 +1,116 @@
-import React from 'react';
-import emailjs from 'emailjs-com';
-import { Container, Form, Input, TextArea, Button } from 'semantic-ui-react'
-
+import React from 'react'
+import emailjs from 'emailjs-com'
 
 const ContactForm = () => {
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const sendEmail = e => {
+    e.preventDefault()
 
-    emailjs.sendForm("default_service", "template_whQB4I7z", e.target, "user_Y0S8nUS9ZEDFDPQgBtEin")
-      .then((result) => {
-          console.log('success');
-      }, (error) => {
-          console.log('failed');
-      });
+    emailjs
+      .sendForm(
+        'default_service',
+        'template_whQB4I7z',
+        e.target,
+        'user_Y0S8nUS9ZEDFDPQgBtEin'
+      )
+      .then(
+        result => {
+          console.log('success')
+        },
+        error => {
+          console.log('failed')
+        }
+      )
   }
 
   return (
-    <Container id='contact-container'>
-      <Form className="contact-form" onSubmit={sendEmail}>
-        <Form.Group widhts='equal'>
-        <Form.Field
-            id='contact-name'
-            control={Input}
-            label='Name'
-            placeholder='Name'
-            name='name'
-          />
-          <Form.Field
-            id='contact-email'
-            control={Input}
-            label='Email'
-            placeholder='Email'
-            name='email'
-          />
-        </Form.Group>
-        <Form.Field
-          id='contact-message'
-          control={TextArea}
-          label='Message'
-          placeholder='Write something..'
-          name='message'
-        />
-        <Form.Field
-          id='form-button-control-public'
-          control={Button}
-          content='Send'
-        />
-      </Form>
-    </Container>
-  );
+    <>
+      <form id="contact-form" onSubmit={sendEmail}>
+        <div className="background">
+          <div onSubmit={sendEmail} class="container">
+            <div className="screen">
+              <div className="screen-header"></div>
+              <div className="screen-body">
+                <div className="screen-body-item left">
+                  <div className="app-title">
+                    <span>CONTACT</span>
+                    <br />
+                    <span>ME</span>
+                  </div>
+                  <div className="app-contact">
+                    CONTACT INFO SWE :<br /> +46 793 40 53 98
+                  </div>
+                </div>
+                <div className="screen-body-item">
+                  <div className="app-form">
+                    <div className="app-form-group">
+                      <input
+                        name="name"
+                        className="app-form-control"
+                        placeholder="ENTER YOUR NAME"
+                      />
+                    </div>
+                    <div className="app-form-group">
+                      <input
+                        name="email"
+                        className="app-form-control"
+                        placeholder="ENTER YOUR EMAIL"
+                      />
+                    </div>
+                    <div className="app-form-group">
+                      <input
+                        name="subject"
+                        className="app-form-control"
+                        placeholder="SUBJECT"
+                      />
+                    </div>
+                    <div className="app-form-group message">
+                      <textarea
+                        name="message"
+                        id="message-field"
+                        className="app-form-control"
+                        placeholder="MESSAGE"
+                      />
+                    </div>
+                    <div className="app-form-group buttons">
+                      <button className="app-form-button">SEND</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="credits">
+              inspired by
+              <a
+                className="credits-link"
+                href="https://dribbble.com/shots/2666271-Contact"
+                target="_blank"
+              >
+                <svg className="dribbble" viewBox="0 0 200 200">
+                  <g stroke="#ffffff" fill="none">
+                    <circle cx="100" cy="100" r="90" stroke-width="20"></circle>
+                    <path
+                      d="M62.737004,13.7923523 C105.08055,51.0454853 135.018754,126.906957 141.768278,182.963345"
+                      stroke-width="20"
+                    ></path>
+                    <path
+                      d="M10.3787186,87.7261455 C41.7092324,90.9577894 125.850356,86.5317271 163.474536,38.7920951"
+                      stroke-width="20"
+                    ></path>
+                    <path
+                      d="M41.3611549,163.928627 C62.9207607,117.659048 137.020642,86.7137169 189.041451,107.858103"
+                      stroke-width="20"
+                    ></path>
+                  </g>
+                </svg>
+                Gururaj
+              </a>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  )
 }
 
 export default ContactForm
