802e66816ae88aba5cf2aaa7472f798e293d524c
jonas <jonas.masharqa@gmail.com>
2020-01-09 00:21:41 +0100

    Adds introduction text for banner.

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -2,11 +2,24 @@ import React from 'react'
 import Synthwave from '../Images/synthwave-banner.jpg'
 
 const LandingPage = () => {
-  const synthwavePic = <img id="synthwave-pic" src={Synthwave} alt="Jonas Picture"/>
+  const synthwavePic = (
+    <img id="synthwave-pic" src={Synthwave} alt="Jonas Picture" />
+  )
   return (
     <>
-      <div id='synth-banner'>
-        {synthwavePic}
+      <div id="synth-banner">
+        <div className="banner-text">
+          <h1 id="banner-header">
+            Front End Developer based in Stockholm, Sweden
+          </h1>
+          <p className="banner-paragraph">
+            Hi! I'm Jonas. Ever since programming caught my interest, I've
+            developed a growing passion for Front End Web Development.
+            <br />
+            Taught from the start to work in an agile way, always in a test driven environemnt.
+          </p>
+        </div>
+        <div>{synthwavePic}</div>
       </div>
     </>
   )
