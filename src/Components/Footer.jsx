186442b400ebaba22ad5e90f6945eaef7082f9d1
jonas <jonas.masharqa@gmail.com>
2020-01-09 17:57:49 +0100

    Adds additional styling for footer.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Icon } from 'semantic-ui-react'
 
 const Footer = () => {
   return (
     <>
-      <section>
-      </section>
       <div class="footer">
-        <div id="button">
-          <div className='pixel'></div>
-        </div>
+        <div id="button"></div>
         <div id="container">
-          <div id="cont">
-            <div className="footer_center">
-              <h3>Classy footer text</h3>
-            </div>
+          <div className="footer-center">
+            <p>
+            <a
+              className="icon-link"
+              href="https://github.com/jonas-masharqa"
+            >
+              <Icon size="large" name="github square" />
+            </a>
+            <a
+              className="icon-link"
+              href="https://se.linkedin.com/in/jonas-masharqa-b759bb18a/%7Bcountry%3Dse%2C+language%3Dsv%7D?trk=people-guest_profile-result-card_result-card_full-click"
+            >
+              <Icon size="large" name="linkedin square" />
+            </a>
+            <br />
+              <NavLink id='footer-nav' to={'/'}>Home</NavLink>
+              <span> :: </span>
+              <NavLink to={'/projects'}>Projects</NavLink>
+              <span> :: </span>
+              <NavLink to={'/about'}>About Me</NavLink>
+              <span> :: </span>
+              <NavLink to={'/contact'}>Contact</NavLink>
+              <p id='copyright'>Copyright © 2020 Jonas Masharqa All Rights Reserved</p>
+            </p>
           </div>
         </div>
       </div>
