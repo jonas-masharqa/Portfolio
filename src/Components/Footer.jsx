de47e986b0b1f9962e3b2a68d565bc6bc9ac5906
jonas <jonas.masharqa@gmail.com>
2020-01-09 15:00:10 +0100

    Adds footer styling.

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <section>
+      </section>
+      <div class="footer">
+        <div id="button">
+          <div className='pixel'></div>
+        </div>
+        <div id="container">
+          <div id="cont">
+            <div className="footer_center">
+              <h3>Classy footer text</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Footer
