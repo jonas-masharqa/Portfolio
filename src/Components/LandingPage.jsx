db31fd1a4d017a6b39ed5bcba8eac284086f0d1f
jonas <jonas.masharqa@gmail.com>
2020-01-11 19:45:38 +0100

    Changes structure of LandingPage and adds additional styling.

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,72 +1,105 @@
 import React from 'react'
-import Synthwave from '../Images/synthwave-banner.jpg'
+import Iframe from './Iframe'
+import Tools from './Tools'
+import SynthBall from '../Images/synth_ball.jpg'
 import RubyLogo from '../Images/ruby_logo.png'
 import ReactLogo from '../Images/react_logo.png'
 import CypressLogo from '../Images/cypress.png'
-import { Grid, Container, Card, Image } from 'semantic-ui-react'
+import { Grid, Container, Card, Image, Header } from 'semantic-ui-react'
 
 const LandingPage = () => {
-  const synthwavePic = (
-    <img id="synthwave-pic" src={Synthwave} alt="Jonas Picture" />
+  const synthwaveBall = (
+    <img id="synthwave-pic" src={SynthBall} alt="Jonas Picture" />
   )
-  const rubyPic = <Image className="card-pic" src={RubyLogo} alt="Ruby on Rails Logo" />
-  const reactPic = <Image className="card-pic" src={ReactLogo} alt="Cypress Logo" />
-  const cypressPic = <Image className="card-pic" src={CypressLogo} alt="Cypress Logo" />
+  const rubyPic = (
+    <Image className="card-pic" src={RubyLogo} alt="Ruby on Rails Logo" />
+  )
+  const reactPic = (
+    <Image className="card-pic" src={ReactLogo} alt="Cypress Logo" />
+  )
+  const cypressPic = (
+    <Image className="card-pic" src={CypressLogo} alt="Cypress Logo" />
+  )
 
   return (
     <>
-      <div id="synth-banner">
+      <div id="iframe-container">
+        <Iframe />
         <div className="banner-text">
           <h1 id="banner-header">
-            Front End Developer based in Stockholm, Sweden
-          </h1>
-          <p className="banner-paragraph">
-            Hi! I'm Jonas. Ever since programming caught my interest, I've
-            developed a growing passion for Front End Web Development.
+            ◪ ◬ ◒ ✕
             <br />
-            Taught from the start to work in an agile way, always in a test
-            driven environemnt.
-          </p>
+            FRONT END
+            <br />
+            DEVELOPER
+            <br />
+            BASED IN STOCKHOLM, SWEDEN
+          </h1>
         </div>
-        <div>{synthwavePic}</div>
       </div>
-      <div id='work'>
-        <p className='work-paragraph'>When working on my projects<br/>I use a variety of programs and dependencies</p>
-      </div>
-      <Container id="landing-container">
-        <Grid id="landing-grid" container columns={3}>
-          <Grid.Column className="landing-column">
-            <Card id="landing-card">
-              <center>{reactPic}</center>
-              <h3 className="card-header">React</h3>
-              <p className="card-text">
-                While writing the Back End parts of my projects, I've been
-                working in the Ruby on Rails framework so far.
-              </p>
-            </Card>
-          </Grid.Column>
-          <Grid.Column className="landing-column">
-            <Card id="landing-card">
-              <center>{rubyPic}</center>
-              <h3 className="card-header">Ruby on Rails</h3>
-              <p className="card-text">
-                While writing the Back End parts of my projects, I've been
-                working in the Ruby on Rails framework so far.
-              </p>
-            </Card>
-          </Grid.Column>
-          <Grid.Column className="landing-column">
-            <Card id="landing-card">
-              <center>{cypressPic}</center>
-              <h3 className="card-header">TDD/BDD</h3>
-              <p className="card-text">
-                While writing the Back End parts of my projects, I've been
-                working in the Ruby on Rails framework so far.
-              </p>
-            </Card>
-          </Grid.Column>
-        </Grid>
+      <Container id="intro-container">
+        <Header id="intro-header" as="h1">
+          Hi! <br />
+          I'm Jonas
+        </Header>
+        <p id="intro-paragraph">
+          Always applying the <br /> agile approach, <br /> while working in a{' '}
+          <br />
+          test driven environment!
+        </p>
+        <p id="intro-paragraph2">
+          Ever since programming caught my interest, I've been advancing in both
+          Front and Backend Web Development.
+          <br />
+          As time went by, I developed a growing passion for the Front End side
+          of the creative process.
+        </p>
       </Container>
+      <div id="work">
+        <p className="work-paragraph">
+          When working on my projects
+          <br />I use a variety of programs and dependencies
+        </p>
+      </div>
+      <br />
+      <div id="synth-banner">
+        <div id="card-banner">{synthwaveBall}</div>
+        <Container id="landing-container">
+          <Grid id="landing-grid" container columns={3}>
+            <Grid.Column className="landing-column">
+              <Card id="landing-card">
+                <center>{reactPic}</center>
+                <h3 className="card-header">ReactJS</h3>
+                <p className="card-text">
+                  While writing the Back End parts of my projects, I've been
+                  working in the Ruby on Rails framework so far.
+                </p>
+              </Card>
+            </Grid.Column>
+            <Grid.Column className="landing-column">
+              <Card id="landing-card">
+                <center>{rubyPic}</center>
+                <h3 className="card-header">Ruby on Rails</h3>
+                <p className="card-text">
+                  While writing the Back End parts of my projects, I've been
+                  working in the Ruby on Rails framework so far.
+                </p>
+              </Card>
+            </Grid.Column>
+            <Grid.Column className="landing-column">
+              <Card id="landing-card">
+                <center>{cypressPic}</center>
+                <h3 className="card-header">TDD/BDD</h3>
+                <p className="card-text">
+                  While writing the Back End parts of my projects, I've been
+                  working in the Ruby on Rails framework so far.
+                </p>
+              </Card>
+            </Grid.Column>
+          </Grid>
+        </Container>
+      </div>
+      <Tools />
     </>
   )
 }
