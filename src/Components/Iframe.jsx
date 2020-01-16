ae938ddd01f3ec43a17b26df148f06adc30327fe
jonas <jonas.masharqa@gmail.com>
2020-01-16 19:16:56 +0100

    Moves iFrame text in to Iframe component.

--- src/Components/Iframe.jsx
+++ src/Components/Iframe.jsx
@@ -3,16 +3,29 @@ import React from 'react'
 const Iframe = () => {
   return (
     <>
-      <div id="iframe-wrapper">
-        <iframe
-          src="//fast.wistia.net/embed/iframe/j7nbk130l6?"
-          frameborder="0"
-          scrolling="no"
-          class="wistia_embed"
-          name="wistia_embed"
-          width="100%"
-          height="1000px"
-        ></iframe>
+      <div id="iframe-container">
+        <div id="iframe-wrapper">
+          <iframe
+            src="//fast.wistia.net/embed/iframe/j7nbk130l6?"
+            frameborder="0"
+            scrolling="no"
+            class="wistia_embed"
+            name="wistia_embed"
+            width="100%"
+            height="1000px"
+          ></iframe>
+        </div>
+        <div className="banner-text">
+          <h1 id="banner-header">
+            ◪ ◬ ◒ ✕
+            <br />
+            FRONT END
+            <br />
+            DEVELOPER
+            <br />
+            BASED IN STOCKHOLM, SWEDEN
+          </h1>
+        </div>
       </div>
     </>
   )
