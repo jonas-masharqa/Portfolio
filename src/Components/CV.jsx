eea1827faeababb1e8c9ff8109ab2eaf0f1fcbe5
jonas <jonas.masharqa@gmail.com>
2020-01-20 21:28:29 +0100

    Adds more to cv data file, fetches in CV component, minor styling.

--- src/Components/CV.jsx
+++ src/Components/CV.jsx
@@ -17,31 +17,84 @@ class CV extends Component {
 
   render() {
     const resume = this.state.resume
-    let goals, education
+    let goals, education, experiences, progLanguages, merits
 
     if (resume) {
       goals = resume.goals.map(goal => {
-        return <li>{goal}</li>
+        return <li className='cv-text' id='cv-text2'>{goal}</li>
       })
     }
 
     if (resume) {
       education = resume.education.map(education => {
-        return <li>{education}</li>
+        return <li className='cv-text' id='cv-text2'>{education}</li>
+      })
+    }
+
+    if (resume) {
+      experiences = resume.experiences.map(experience => {
+        return <li className='cv-text' id='cv-text2'>{experience}</li>
+      })
+    }
+
+    if (resume) {
+      progLanguages = resume.progLanguages.map(progLanguage => {
+        return <li className='cv-text' id='cv-text2'>{progLanguage}</li>
+      })
+    }
+
+    if (resume) {
+      merits = resume.merits.map(merit => {
+        return <li className='cv-text' id='cv-text2'>{merit}</li>
       })
     }
 
     return (
       <>
-        <Container text>
-          <h1>CV</h1>
-          <h2>Jonas Masharqa</h2>
+        <br />
+        <br />
+        <Container id='cv'>
+          <h1 className='cv-text' id='cv-header'>Jonas Masharqa</h1>
           <br />
-          <h2>My Goals</h2>
+          <h2 className='cv-text'>My Goals</h2>
           {goals}
           <br />
-          <h2>Education</h2>
+          <h2 className='cv-text'>Education</h2>
           {education}
+          <h2 className='cv-text'>Professional Experience</h2>
+          {experiences}
+          <h2 className='cv-text'>Programming Languages & Frameworks/Libraries etc</h2>
+          {progLanguages}
+          <h2>Languages</h2>
+          <li className='cv-text' id='cv-text2'>Swedish - Native language</li>
+          <li className='cv-text' id='cv-text2'>
+            English - Very good knowledge both grammatically and speech wise on
+            a professional level.
+          </li>
+          <li className='cv-text' id='cv-text2'>Thai - OK perception of both speech and understanding.</li>
+          <h2 className='cv-text'>Other Merits</h2>
+          {merits}
+          <h2 className='cv-text'>Interests</h2>
+          <p className='cv-text' id='cv-text2'>
+            My interests consist of seeing my friends, playing and listening to
+            music, exercising, and traveling.
+          </p>
+          <h2 className='cv-text'>Characteristics</h2>
+          <p className='cv-text' id='cv-text2'>
+            Eager to learn. A team player who can also take command when needed
+            and find suitable solutions.
+            <br />
+            <br />
+            I’m strong in creating and maintaining customer relationships.
+            <br />
+            <br />
+            During my experiences working, I’ve learned how important it is to
+            keep a positive atmosphere at your workplace, which is something I
+            consider myself having gotten especially good at during my time as a
+            salesman.
+          </p>
+          <h2 className='cv-text'>References</h2>
+          <p className='cv-text' id='cv-text2'>References can be given on request.</p>
         </Container>
       </>
     )
